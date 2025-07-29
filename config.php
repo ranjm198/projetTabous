389e1b5aa173efad558d24ad2c98074f44befbe1
unknown <ranjm198@gmail.com>
2025-07-30 01:12:24 +0200

    Second committtt453

--- config.php
+++ config.php
@@ -1,20 +1,19 @@
 <?php
-$host   = getenv('DB_HOST');
-$db     = getenv('DB_DATABASE') ?: getenv('MYSQL_DATABASE');
-$user   = getenv('DB_USER') ?: getenv('MYSQL_USER');
-$pass   = getenv('DB_PASSWORD') ?: getenv('MYSQL_PASSWORD');
-$port   = getenv('DB_PORT') ?: '3306';
+$host = 'localhost';
+$db   = 'facture';
+$user = 'root';
+$pass = '';
 $charset = 'utf8mb4';
 
-$dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
 ];
 
 try {
-    $pdo = new PDO($dsn, $user, $pass, $options);
+     $pdo = new PDO($dsn, $user, $pass, $options);
 } catch (\PDOException $e) {
-    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+     throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
 ?>
