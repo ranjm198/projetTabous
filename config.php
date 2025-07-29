ad12e7b29ac3e0d668d123a9bbf96fa2b9fd1741
unknown <ranjm198@gmail.com>
2025-07-30 01:34:27 +0200

    Second comm27

--- config.php
+++ config.php
@@ -11,7 +11,7 @@ $options = [
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
 ];
 
-try {
+try { 
      $pdo = new PDO($dsn, $user, $pass, $options);
 } catch (\PDOException $e) {
      throw new \PDOException($e->getMessage(), (int)$e->getCode());
