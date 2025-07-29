488c0e8aff9704ee121f5fb17162b2d49f7bc8ea
unknown <ranjm198@gmail.com>
2025-07-30 01:57:58 +0200

    Second ranya7

--- config.php
+++ config.php
@@ -3,17 +3,16 @@ $host = getenv('DB_HOST');
 $db   = getenv('DB_NAME');
 $user = getenv('DB_USER');
 $pass = getenv('DB_PASS');
-$charset = 'utf8mb4';
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$dsn = "pgsql:host=$host;port=5432;dbname=$db";
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
 ];
 
-try { 
+try {
      $pdo = new PDO($dsn, $user, $pass, $options);
 } catch (\PDOException $e) {
-     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+     die("Erreur de connexion à la base : " . $e->getMessage());
 }
 ?>
